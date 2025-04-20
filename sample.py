7bef199e8263282b21c058a0b306cb49a47c5873
Paul Fontaine <pfontaine@etu.uqac.ca>
2025-04-20 18:04:10 -0400

    set cfg_scale with an argument

--- sample.py
+++ sample.py
@@ -3,17 +3,19 @@ from torch.utils.checkpoint import checkpoint
 from config import CONFIG
 from unetv2 import UNet
 from diffusion import GaussianDiffusion
-import torch
 from torchvision.utils import save_image
 import os
+import argparse
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Sample 5 images per class using the diffusion model and save them in the samples folder.")
+    parser.add_argument("-g", "--cfg_scale", type=float, default=CONFIG.DIFFUSION.guidance_strength,
+                        help="CFG guidance strength (default: value from CONFIG).")
+    args = parser.parse_args()
+
     device = CONFIG.device
-    if device.type == 'cuda':
-        checkpoint = "unet_weights_cuda.pt"
-    elif device.type == 'cpu':
-        checkpoint = "unet_weights_cpu.pt"
+    checkpoint = f"unet_weights_{CONFIG.DATASET.name}_{device.type}.pt"
 
     model = UNet(*CONFIG.MODEL.get_params()).to(device)
     model.load_from_pretrained(checkpoint, device=device)
@@ -23,3 +25,4 @@ if __name__ == "__main__":
     grid = diffusion.sample()
     save_name = os.path.join("samples", f"samples_{CONFIG.DATASET.name}_cfg={CONFIG.DIFFUSION.guidance_strength}.png")
     save_image(grid, save_name, nrow=CONFIG.DATASET.num_classes, normalize=True)
+    print(f"Sampled images saved to {save_name}")
